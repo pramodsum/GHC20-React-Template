3031d791b0cbba7964932f5faae5f193861d5b09
Sumedha Pramod <sumedha.pramod@gmail.com>
2020-03-31 10:54:52 -0400

    Move types into separate file

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { Card, CardContent } from "@material-ui/core";
 import ReactMarkdown from "react-markdown";
-
-import "./ItemCard.css";
 import { Link } from "react-router-dom";
 
-export type Item = {
-  name: string;
-  slug: string;
-  recipe?: string;
-  url?: string;
-  uuid?: string;
-};
+import "./ItemCard.css";
+import { Item } from "../../utils/types";
 
 const shortenString = (str: string, maxLen: number, separator = " ") => {
   if (str.length <= maxLen) return str;
--- src/routes/ItemDetailsPage.tsx
+++ src/routes/ItemDetailsPage.tsx
@@ -1,67 +1,13 @@
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
-import ReactMarkdown from "react-markdown";
-import { TextField, Box, Typography, Button } from "@material-ui/core";
 
 import Layout from "../components/Layout/Layout";
-import { TACO_API_BASE, RouteParams } from "../utils/globals";
-
-type ItemDetails = {
-  name: string;
-  slug: string;
-  recipe: string;
-  url: string;
-};
+import { RouteParams } from "../utils/types";
 
 const ItemDetailsPage: React.FC<RouteComponentProps> = ({ match }) => {
   const { slug } = match.params as RouteParams;
-  const [itemDetails, setItemDetails] = React.useState<ItemDetails>();
 
-  React.useEffect(() => {
-    fetch(`${TACO_API_BASE}/toppings/${slug}/`)
-      .then(response => response.json())
-      .then(setItemDetails);
-  }, [slug]);
-
-  const [review, updateReview] = React.useState<string>();
-
-  return (
-    <Layout>
-      {itemDetails && (
-        <>
-          <h1>{itemDetails.name}</h1>
-          <ReactMarkdown source={itemDetails.recipe} />
-        </>
-      )}
-      <Box>
-        <Typography variant="h6">Reviews</Typography>
-        <Box display="flex" justifyContent="space-between">
-          <TextField
-            variant="outlined"
-            fullWidth
-            multiline
-            placeholder="Review this taco Item"
-            style={{ marginRight: "10px" }}
-            value={review}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-              updateReview(event.target.value)
-            }
-          />
-          <Button
-            color="primary"
-            variant="contained"
-            style={{ width: "200px", maxHeight: "56px" }}
-            onClick={() => {
-              updateReview("");
-              window.alert(`New review: \n${review}`);
-            }}
-          >
-            Submit Review
-          </Button>
-        </Box>
-      </Box>
-    </Layout>
-  );
+  return <Layout>{slug}</Layout>;
 };
 
 export default ItemDetailsPage;
